cfe707f8b17a63e25d99e90a56088bc9b9f7b5f5
Stanislav Kapulkin <kapulkin@gmail.com>
2025-05-29 18:35:11 +0300

    [*] version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="promptbuilder",
-    version="0.3.11",
+    version="0.4.0",
     packages=find_packages(),
     install_requires=[
         "pydantic",
