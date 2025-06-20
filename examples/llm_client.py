64accdd1b51a75a61d0f4d6d2339eb596480a21a
Stanislav Kapulkin <kapulkin@gmail.com>
2025-06-21 02:29:31 +0300

    Revert "[*] json parsing is moved to create_value for aisuit"
    This reverts commit 39565c5932f7923b95d283eb06f86d571fe79145.

--- examples/llm_client.py
+++ examples/llm_client.py
@@ -1,28 +1,9 @@
-#%%
-%cd ..
-#%%
 from promptbuilder.llm_client import get_client
-from promptbuilder.llm_client.types import Content, Part
 import os
 import dotenv
-from pydantic import BaseModel
 
 dotenv.load_dotenv()
-
-class City(BaseModel):
-    city: str
-
-# model = "openai:gpt-4o-mini"
-# api_key = os.getenv('OPENAI_API_KEY')
-model = "google:gemini-2.0-flash"
-api_key = os.getenv('GOOGLE_API_KEY')
-llm_client = get_client(full_model_name=model, api_key=api_key)
-response = llm_client.create(
-    [Content(role="user", parts=[Part(text="What is the capital of France?")])],
-    result_type=City,
-    max_tokens=2
-)
-print(response.candidates[0].content.parts[0].text)
-print(response.candidates[0].finish_reason)
-
-# %%
+model = "openai:gpt-4o-mini"
+api_key = os.getenv('OPENAI_API_KEY')
+llm_client = get_client(model=model, api_key=api_key)
+print(llm_client.from_text("What is the capital of France?"))
