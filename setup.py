537b9400aec2011c2298a0601207b6f5870f3c29
Stanislav Kapulkin <kapulkin@gmail.com>
2025-07-04 14:25:45 +0300

    [*] new version for generalized exception handling

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="promptbuilder",
-    version="0.4.19",
+    version="0.4.20",
     packages=find_packages(),
     install_requires=[
         "pydantic",
